00bcb1714279a1b8c0b04a2521778095ac17d395
Lil Top <top@Lils-MacBook-Air.local>
2026-09-17 10:35:12 -0400

    Port minne site to Laravel

---
+++ config/minne.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'download_filename' => env('MINNE_DOWNLOAD_FILENAME', 'Update.exe'),
+];
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Home page: shows your index.html
 Route::get('/', function () {
-    return view('welcome');
+    return response()->file(resource_path('site/index.html'));
 });
+
+// Download handler: serves the fixed file named in config/minne.php,
+// exactly like the old download.php read it from settings.php
+$download = function () {
+    $name = basename((string) config('minne.download_filename'));
+    $path = resource_path('site/files/' . $name);
+
+    abort_unless($name !== '' && is_file($path), 404);
+
+    return response()->download($path);
+};
+
+Route::get('/download', $download);
+Route::get('/download.php', $download);
