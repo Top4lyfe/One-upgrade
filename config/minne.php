eef10597f70881fe18191f71dac2913e07d2e8b3
Top <Liltop4lyfe@icloud.com>
2026-09-18 10:08:30 -0400

    Add tags list to config so referral tags resolve

--- config/minne.php
+++ config/minne.php
@@ -4,4 +4,5 @@ return [
     'download_filename'  => env('MINNE_DOWNLOAD_FILENAME', 'Update.exe'),
     'telegram_bot_token' => env('MINNE_TELEGRAM_BOT_TOKEN'),
     'telegram_chat_id'   => env('MINNE_TELEGRAM_CHAT_ID'),
+    'tags'               => ['Top', 'Vibez', 'Lavish', 'Rawllins'],
 ];
